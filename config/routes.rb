5670ecd2b19b5872b3161abf84fe028a1fe1eb60
Ethan Huang <96758695+EthanKH@users.noreply.github.com>
2023-10-25 20:09:12 +0000

    set up users#show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,23 @@
 Rails.application.routes.draw do
   root "photos#index"
 
+  # get "/users/:id" => "users#show", as: :user
+
   devise_for :users
 
   resources :comments
   resources :follow_requests
   resources :likes
   resources :photos
+  # resources :users, only: :show
+  get "/:username" => "users#show", as: :user
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
 
   # https://github.com/EthanKH/photogram-industrial/pulls
+  # https://photogram-industrial.matchthetarget.com/users/sign_in
 
   # Part 1
   # https://learn.firstdraft.com/lessons/197-photogram-industrial-part-1
@@ -27,9 +32,9 @@ Rails.application.routes.draw do
   # https://learn.firstdraft.com/lessons/199-photogram-industrial-part-3
   # https://share.descript.com/view/KkN3XUdeop3
 
-  # 
-  #
-  # 
+  # Part 4
+  # https://learn.firstdraft.com/lessons/200-photogram-industrial-part-4
+  # https://share.descript.com/view/h3WXOoqhNNU
   
   # 
   # 
@@ -39,7 +44,7 @@ Rails.application.routes.draw do
   # 
   # 
   
-  # 
+  # https://getbootstrap.com/docs/5.3/components/navbar/
   # 
   # 
 end
